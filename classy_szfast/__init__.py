1c0585b1abe3ad868d3ca8214aaa9bc065b5e89e
Boris Bolliet <boris.bolliet@gmail.com>
2026-03-01 17:17:32 +0100

    Remove halo_model module and imports
    Delete the large pure-JAX halo_model implementation (classy_szfast/halo_model.py) and remove its imports from classy_szfast/__init__.py. Also remove unused stdlib imports (Path, sys) and clean up __init__.py exports so the package no longer re-exports halo_model symbols. This reduces package surface and removes the now-untracked/obsolete halo_model implementation.

--- classy_szfast/__init__.py
+++ classy_szfast/__init__.py
@@ -4,18 +4,10 @@ from .utils import *
 from .cosmopower import *
 from .pks_and_sigmas import *
 
-from pathlib import Path
-import sys
-
 from .utils import Const
 
-
 from .custom_profiles.custom_profiles import *
-
-
 from .custom_bias.custom_bias import *
-
-from .halo_model import cl_yy, prepare_grids, arnaud10_profile_x, duffy08_cvir
 
 from .cosmology import CosmoGrids, build as build_cosmo_grids
 from .hmf import HaloGrids, build_halo_grids, tinker08_hmf, tinker10_bias
